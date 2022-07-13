9a0fdc3108eae7225d933656605a93695e29e9dd
ISAAC <66412254+isaacmobe@users.noreply.github.com>
2022-07-13 17:31:39 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/*===========================TYING ANIMATION ==============================*/
+var typed = new Typed(".typing", {
+    strings:["","Web Designer", "web Developer", "Graphic Designer", "Artist"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
